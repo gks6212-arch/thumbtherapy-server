9c868ac6f0b82241db560acf95f65b40254bc6bd
gks6212-arch <gks6212@gmail.com>
2026-04-04 16:02:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.send("server running");
 });
